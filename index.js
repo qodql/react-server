beb15aff6f0048dcf137eae4f55c3806e0e206d1
ykb <qodqlqodql1206@gmail.com>
2024-09-06 17:45:04 +0900

    todos

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const todos = require('./api/todos');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/todos',todos);
+
+
+app.listen(4000);
