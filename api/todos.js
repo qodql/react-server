bb14bb8725cc09c8a8b26c310c886cb6d718c2ff
ykb <qodqlqodql1206@gmail.com>
2024-09-09 14:08:45 +0900

    server

--- api/todos.js
+++ api/todos.js
@@ -1,49 +1,66 @@
 const express = require('express');
-const fs = require('fs');
-const todos = express.Router()
+const todos = express.Router();
 
+const { MongoClient } = require('mongodb');
+const dbName = 'todos';
+const url = 'mongodb+srv://qodqlqodql1206:GKq2TaiXWVB4oSzr@cluster0.fiufo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const client = new MongoClient(url);
 
-let data = fs.readFileSync('./db/data.json');
-let dataParse = JSON.parse(data);
+async function connect(){
+    await client.connect();
+    const db = client.db(dbName);
+    
+    return db.collection('data');
+}
 
-todos.get('/', function (req, res) {
-    console.log(dataParse)
-  res.send( dataParse )
-})
-todos.get('/:id', function (req, res) {
-    let {id} = req.params;
-    let d = dataParse.list.filter((obj)=>obj.id == id);
+todos.get('/', async function (req, res) {
+    const collection = await connect();
+    const findResult = await collection.find({}).toArray();
+    client.close();
 
-    res.send(d)
-})
-
-todos.post('/', function (req, res) {
-    let body = [...dataParse.list, req.body];
-    fs.writeFileSync('./db/data.json',JSON.stringify({list:body}))
-    res.send({list:body})
+    res.send( findResult )
 })
 
+todos.get('/:id', async function (req, res) {
+    let id = req.params;
+    
+    const collection = await connect();
+    const findResult = await collection.find(id).toArray();
+    client.close();
 
-todos.put('/', function (req, res) {
-    let {id,status} = req.body;  
-
-    let body = [...dataParse.list].map(obj=>{
-        if(obj.id == id){
-            obj.status = status;
-        }
-        return obj;
-    })
-
-    fs.writeFileSync('./db/data.json',JSON.stringify({list:body}))
-    res.send({list:body})
+    res.send( findResult )
 })
 
-todos.delete('/', function (req, res) {
-    let {id} = req.query;    
-    let body = [...dataParse.list].filter(obj=>obj.id != id);
+todos.post('/', async function (req, res) {
 
-    fs.writeFileSync('./db/data.json',JSON.stringify({list:body}))
-    res.send({list:body})
+    const collection = await connect();
+                       await collection.insertOne(req.body);
+    const findResult = await collection.find({}).toArray();
+    client.close();
+   
+    res.send(findResult)
 })
+
+
+todos.put('/', async function (req, res) {
+    const collection = await connect();
+                       await collection.updateOne({id:req.body.id}, {$set:req.body});
+    const findResult = await collection.find({}).toArray();
+    client.close();
+   
+    res.send(findResult)
+
+})
+
+todos.delete('/', async function (req, res) {
+      
+    const collection = await connect();
+                       await collection.deleteOne(req.query);
+    const findResult = await collection.find({}).toArray();
+    client.close();
+
+    res.send(findResult)
+})
+
 
 module.exports = todos;
