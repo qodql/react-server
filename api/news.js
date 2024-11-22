4227991844ed22b7e9175db4e970f05c5be1a80d
ykb <qodqlqodql1206@gmail.com>
2024-11-22 10:36:36 +0900

    news date

--- api/news.js
+++ api/news.js
@@ -6,7 +6,7 @@ news.get('/', async function (req, res){
 
     const {m, s} = req.query;
 
-    const api = await axios.get(`https://api-v2.deepsearch.com/v1/${m}/${s}?date_from=2024-10-10&date_to=2024-10-24&api_key=5b471a9c36214c4496843a1d5c6e5e2b`);
+    const api = await axios.get(`https://api-v2.deepsearch.com/v1/${m}/${s}&api_key=5b471a9c36214c4496843a1d5c6e5e2b`);
     res.json(api.data);
 })
 
